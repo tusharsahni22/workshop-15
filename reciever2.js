d5b356e5cc0280b6440f9cedaaf59e106e4c8aa4
Tushar Sahni <tusharsahni22@gmail.com>
2021-08-03 19:24:23 +0530

    initial commit

---
+++ reciever2.js
@@ -0,0 +1,21 @@
+const amqp = require (`amqplib/callback_api`);
+
+amqp.connect(`amqp://localhost`,(err,connection)=>{
+    if(err){
+        throw err;
+    }
+    connection.createChannel((err,channel)=>{
+        if (err){
+            throw err;
+        }
+        let queueName = "test";
+        
+        channel.assertQueue(queueName,{
+            durable: false
+        });
+       channel.consume(queueName,(msg)=>{
+           console.log(`Recieved : ${msg.content.toString()}`);
+           channel.ack(msg);
+        })
+    })    
+});      
